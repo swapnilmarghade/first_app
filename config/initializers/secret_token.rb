11b99423e1b4a2f9d2f6fd67992cfd0ea1e60feb
swapnilm <swapnilm@flipkart.com>
2013-09-18 23:02:29 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '3d147c3910786b1964aa885a1ef14539ea43d252f047782bd0cde720aa439256f178a0da9bd9077d290304f08e34964ff6865a66f7eb278149595df3f3a13842'
